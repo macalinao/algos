34091eafbf185f43eec44111797cf87c1bf06b17
Ian Macalinao <me@ian.pw>
2014-11-11 07:32:16 -0600

    allow arrays of odd values

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -3,8 +3,10 @@ import java.util.*;
 public class MergeSort {
   public static void main(String[] args) {
     int[] toSort = new int[]{5, 3, 2, 8, 1, 4, 7, 6};
+    int[] toSortOdd = new int[]{5, 3, 2, 8, 1, 9, 4, 7, 6};
 
     System.out.println(Arrays.toString(mergeSort(toSort)));
+    System.out.println(Arrays.toString(mergeSort(toSortOdd)));
   }
 
   public static int[] mergeSort(int[] unsorted) {
@@ -12,11 +14,13 @@ public class MergeSort {
       return new int[] {Math.min(unsorted[0], unsorted[1]), Math.max(unsorted[0], unsorted[1])};
     }
 
+    boolean odd = unsorted.length % 2 != 0;
+    int oddVal = unsorted[unsorted.length / 2];
     int[] a = new int[unsorted.length / 2];
     int[] b = new int[unsorted.length / 2];
     for (int i = 0; i < unsorted.length / 2; i++) {
       a[i] = unsorted[i];
-      b[i] = unsorted[unsorted.length - (unsorted.length / 2 - i)];
+      b[i] = unsorted[i + (odd ? 1 : 0) + unsorted.length / 2];
     }
 
     a = mergeSort(a);
@@ -26,13 +30,37 @@ public class MergeSort {
 
     int ap = 0;
     int bp = 0;
+    boolean oddUsed = false;
 
     for (int i = 0; i < sorted.length; i++) {
-      if (bp >= b.length || (ap < a.length && a[ap] < b[bp])) {
-        sorted[i] = a[ap++];
+      boolean aOut = ap >= a.length;
+      boolean bOut = bp >= b.length;
+
+      if (bOut) {
+        if (odd && a[ap] > oddVal && !oddUsed) {
+          sorted[i] = oddVal;
+          oddUsed = true;
+        } else {
+          sorted[i] = a[ap++];
+        }
+      } else if (aOut) {
+        if (odd && b[bp] > oddVal && !oddUsed) {
+          sorted[i] = oddVal;
+          oddUsed = true;
+        } else {
+          sorted[i] = b[bp++];
+        }
       } else {
-        sorted[i] = b[bp++];
+        if (odd && a[ap] > oddVal && b[bp] > oddVal && !oddUsed) {
+          sorted[i] = oddVal;
+          oddUsed = true;
+        } else if (a[ap] < b[bp]) {
+          sorted[i] = a[ap++];
+        } else {
+          sorted[i] = b[bp++];
+        }
       }
+
     }
 
     return sorted;
